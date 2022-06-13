c19fe9a6db90bb058091ba6aae9da3b99c2862f2
mjarmak <mohamadjarmak@gmail.com>
2022-06-13 22:20:52 +0200

    used enhanced for loop in GlidedRose.updateQuality for better readability

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -11,60 +11,60 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE.toString())
-                    && !items[i].name.equals(BACKSTAGE.toString())) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS.toString())) {
-                        if (!items[i].name.equals(CONJURED.toString())) {
-                            items[i].quality = items[i].quality - 1;
+        for (Item item : items) {
+            if (!item.name.equals(AGED_BRIE.toString())
+                && !item.name.equals(BACKSTAGE.toString())) {
+                if (item.quality > 0) {
+                    if (!item.name.equals(SULFURAS.toString())) {
+                        if (!item.name.equals(CONJURED.toString())) {
+                            item.quality = item.quality - 1;
                         } else {
-                            items[i].quality = items[i].quality - 2;
+                            item.quality = item.quality - 2;
                         }
                     }
                 }
             } else {
-                if (items[i].quality < maxQuality) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < maxQuality) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals(BACKSTAGE.toString())) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < maxQuality) {
-                                items[i].quality = items[i].quality + 1;
+                    if (item.name.equals(BACKSTAGE.toString())) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < maxQuality) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < maxQuality) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < 6) {
+                            if (item.quality < maxQuality) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals(SULFURAS.toString())) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.name.equals(SULFURAS.toString())) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE.toString())) {
-                    if (!items[i].name.equals(BACKSTAGE.toString())) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS.toString())) {
-                                if (!items[i].name.equals(CONJURED.toString())) {
-                                    items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!item.name.equals(AGED_BRIE.toString())) {
+                    if (!item.name.equals(BACKSTAGE.toString())) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals(SULFURAS.toString())) {
+                                if (!item.name.equals(CONJURED.toString())) {
+                                    item.quality = item.quality - 1;
                                 } else {
-                                    items[i].quality = items[i].quality - 2;
+                                    item.quality = item.quality - 2;
                                 }
                             }
                         }
                     } else {
-                        items[i].quality = 0;
+                        item.quality = 0;
                     }
                 } else {
-                    if (items[i].quality < maxQuality) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < maxQuality) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
